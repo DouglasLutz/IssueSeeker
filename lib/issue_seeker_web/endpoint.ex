170ad835e897ef2acc4ae4ab7c2c6995aca8007a
Douglas Lutz <douglasdiehllutz@hotmail.com>
2020-12-09 21:39:13 -0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/issue_seeker_web/endpoint.ex
+++ lib/issue_seeker_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule IssueSeekerWeb.Endpoint do
   ]
 
   socket "/socket", IssueSeekerWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
